d5f59e9202147eebb6a7c95896d6b026dc88c454
Javier <javiermvm6@gmail.com>
2024-11-14 01:52:12 -0600

    chapter 1-2 starting chapter 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+import {inter, roboto} from "@/app/ui/fonts";
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${roboto.className} antialising`}>{children}</body>
     </html>
   );
 }
